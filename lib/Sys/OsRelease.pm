5359904f91d37704d7968d722eb7baeff94aeb05
Ian Kluft <github-dev@iankluft.com>
2022-04-24 23:49:05 -0700

    more initial directory setup

--- lib/Sys/OsRelease.pm
+++ lib/Sys/OsRelease.pm
@@ -1,3 +1,8 @@
+# Sys::OsRelease
+# ABSTRACT: helper library in Perl for reading OS info from FreeDesktop.Org-standard /etc/os-release file
+# Copyright (c) 2022 by Ian Kluft
+# Open Source license under the same terms as Perl itself: <http://www.perl.com/perl/misc/Artistic.html>
+
 use strict;
 use warnings;
 package Sys::OsRelease;
