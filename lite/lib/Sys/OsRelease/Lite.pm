c9dfe02d58317c20ae8d764fbf6170149137b5eb
Ian Kluft <ko6yq.dev+github@gmail.com>
2026-07-14 16:52:20 -0700

    version template no longer needed since Makefile calls get_semver.pl

--- lite/lib/Sys/OsRelease/Lite.pm
+++ lite/lib/Sys/OsRelease/Lite.pm
@@ -4,13 +4,11 @@
 # Copyright (c) 2026 by Ian Kluft
 # Open Source license Perl's Artistic License 2.0: <http://www.perlfoundation.org/artistic_license_2_0>
 # SPDX-License-Identifier: Artistic-2.0
-use v5.10;
+
+package Sys::OsRelease::Lite;
 use strict;
 use warnings;
 use utf8;
-
-package Sys::OsRelease::Lite;
-$Sys::OsRelease::Lite::VERSION = '%VERSION%';
 
 1;
 
